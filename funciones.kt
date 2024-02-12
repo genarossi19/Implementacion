aefd5d1f5490916620ecac8545c78d8e5cf5daff
Genaro Rossi <89028312+genarossi19@users.noreply.github.com>
2024-02-12 20:52:36 -0300

    funciones

---
+++ funciones.kt
@@ -0,0 +1,16 @@
+package com.genarossi19.helloworld
+
+fun main(args: Array<String>) {
+    showMyName()
+    showMyLastName()
+    showMyAge()
+}
+fun showMyName(){
+    println("Me llamo Genaro")
+}
+fun showMyLastName(){
+    println("Mi Apellido es Rossi")
+}
+fun showMyAge(){
+    println("Tengo 20 años")
+}
